1238c3d43cbde3f1eb029b2dd561d2b313a865e5
Akash7899 <aka7899akash@gmail.com>
2022-12-15 22:45:17 +0530

    Law for me setup

---
+++ src/layout/headerTop.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const HeaderTop = () => {
+  return (
+    <>
+      <div className="headerTop">
+        <div className="container">
+          <div className="row">
+            <div className="Wrapper">
+              <div className="wrapperInner">
+                <div className="wrapperin>">
+                <img src={require('../assets/images/Vector.png')} />
+                  <span>
+                    <a href="">Some Offer / CTA goes here / Lorem Ipsum </a>
+                  </span>
+                </div>
+                <div className="wrapperin>">
+                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HeaderTop;
